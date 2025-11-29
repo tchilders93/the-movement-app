ec7306894f963379a6da097ef0268031730ba615
tchilders93 <tchilders93@gmail.com>
2025-11-29 13:33:37 -0500

    Refactor Home component to display movement patterns

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import Link from "next/link";
+import movementsData from "../public/data/movements.json";
+
 export default function Home() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Movement App</h1>
-      <p>Welcome! Use the link below to explore all movements.</p>
+      <p>Select a movement pattern:</p>
 
-      <a href="/movements" style={{ color: "blue", textDecoration: "underline" }}>
-        Go to Movements
-      </a>
+      <ul>
+        {movementsData.movement_patterns.map((movement, index) => (
+          <li key={index}>
+            <Link href={`/movements/${index}`}>
+              {movement.movement_pattern}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <br />
+      <Link href="/movements">
+        <button>View All Movements</button>
+      </Link>
     </div>
   );
 }
+
