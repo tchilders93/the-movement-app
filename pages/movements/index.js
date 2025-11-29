ab4e1d848c210e1705c9408c313e6aaff4b0f347
tchilders93 <tchilders93@gmail.com>
2025-11-29 13:44:47 -0500

    Refactor MovementsIndex to use anchor tags
    Updated the movement list to use anchor tags for navigation and changed the key for list items.

--- pages/movements/index.js
+++ pages/movements/index.js
@@ -1,27 +1,23 @@
-import Link from "next/link";
 import movementsData from "../../public/data/movements.json";
 
 export default function MovementsIndex() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>All Movement Patterns</h1>
-      <p>Choose a movement to view regressions & progressions:</p>
 
       <ul>
-        {movementsData.movement_patterns.map((movement, index) => (
-          <li key={index}>
-            <Link href={`/movements/${index}`}>
+        {movementsData.movement_patterns.map((movement) => (
+          <li key={movement.movement_pattern}>
+            <a
+              href={`/movements/${encodeURIComponent(
+                movement.movement_pattern
+              )}`}
+            >
               {movement.movement_pattern}
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
-
-      <br />
-      <Link href="/">
-        <button>Back to Home</button>
-      </Link>
     </div>
   );
 }
-
